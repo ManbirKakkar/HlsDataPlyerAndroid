78205ab9eab18bbc9c8310d06185e86e1486f729
ManbirKakkar <kakkar.manbir11@gmail.com>
2025-05-30 13:46:04 -0700

    Shaka player

--- app/src/main/java/com/mk/hls/shaka/ShakaPlayerActivity.kt
+++ app/src/main/java/com/mk/hls/shaka/ShakaPlayerActivity.kt
@@ -1,53 +1,45 @@
 package com.mk.hls.shaka
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.mk.hls.R
-import com.mk.hls.ui.MainActivity.Companion.SELECTED_URL
-import java.net.URLEncoder
+import com.mk.hls.ui.MainActivity.Companion.TEST_URL
 
 class ShakaPlayerActivity : AppCompatActivity() {
 
+    private lateinit var webView: WebView
+    private val hlsUrl = TEST_URL
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shaka_player)
 
-        val webView = findViewById<WebView>(R.id.webView).apply {
-            settings.apply {
-                javaScriptEnabled = true
-                mediaPlaybackRequiresUserGesture = false
-                allowFileAccessFromFileURLs = true
-                allowUniversalAccessFromFileURLs = true
-                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
-            }
+        webView = findViewById(R.id.webView)
 
-            WebView.setWebContentsDebuggingEnabled(true)
+        with(webView.settings) {
+            javaScriptEnabled = true
+            mediaPlaybackRequiresUserGesture = false
+            cacheMode = WebSettings.LOAD_NO_CACHE
+        }
 
-            webChromeClient = WebChromeClient() // enable console logs, fullscreen, etc.
-            webChromeClient = object : WebChromeClient() {
-                override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
-                    Log.d(
-                        "ShakaWebView",
-                        "JS ${consoleMessage?.messageLevel()} @ ${consoleMessage?.sourceId()}:${consoleMessage?.lineNumber()} – ${consoleMessage?.message()}"
-                    )
-                    return true
-                }
-
-                // For fullscreen video support, if you need it
-                override fun onShowCustomView(view: View?, callback: CustomViewCallback?) { /* … */ }
-                override fun onHideCustomView() { /* … */ }
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                webView.evaluateJavascript("setVideoUrl('$hlsUrl');", null)
             }
         }
 
-        // Build a file:// URL with query param
-        val encoded = URLEncoder.encode(SELECTED_URL, "UTF-8")
-        val url = "file:///android_asset/shaka_player.html?streamUrl=$encoded"
-        webView.loadUrl(url)
+        webView.loadUrl("file:///android_asset/shaka_player.html")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        webView.destroy()
     }
 }
--- app/src/main/java/com/mk/hls/ui/MainActivity.kt
+++ app/src/main/java/com/mk/hls/ui/MainActivity.kt
@@ -21,6 +21,8 @@ class MainActivity : AppCompatActivity(), StreamListFragment.StreamSelectionList
 
     companion object {
         var SELECTED_URL: String = ""
+        var TEST_URL =
+            "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1747716945/ei/8bYraNCLD_zRsfIP5ZTLuAY/ip/2601:642:4f7c:b9e0:fd81:66d9:292f:5eaf/id/750c38c3d5a05dc4/itag/232/source/youtube/requiressl/yes/ratebypass/yes/pfa/1/sgovp/clen%3D16782510%3Bdur%3D212.040%3Bgir%3Dyes%3Bitag%3D136%3Blmt%3D1717051830491480/rqh/1/hls_chunk_host/rr1---sn-o097znz7.googlevideo.com/xpc/EgVo2aDSNQ%3D%3D/met/1747695345,/mh/7c/mm/31,29/mn/sn-o097znz7,sn-n4v7snse/ms/au,rdu/mv/m/mvi/1/pl/34/rms/au,au/initcwndbps/4098750/bui/AecWEAY7gol-MBQi7EsmmThVIDWGs5QPomoq1FDRlpnjFseykT3YmDrP0lqPSw9W4KMepLrIFLnUiLdc/spc/wk1kZhYhWVI5Sy_nHIALBiJLmJCAC89WyzLfkCMMlJQ-RfpQY3M/vprv/1/playlist_type/DVR/dover/13/txp/4535434/mt/1747694947/fvip/2/short_key/1/keepalive/yes/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,pfa,sgovp,rqh,xpc,bui,spc,vprv,playlist_type/sig/AJfQdSswRAIgbUfetv7iyYzkOe3vYXSYGkdRTmbXAZXm6BUgXrM4mOwCIC2bojDg_KB6vaEE1vKW21363tquri6Smmq4G1K0lOxY/lsparams/hls_chunk_host,met,mh,mm,mn,ms,mv,mvi,pl,rms,initcwndbps/lsig/ACuhMU0wRQIhAJLGbhiN-PAxC0WCIbPJgUp_0G5VNvn9FTJnexKH8xV-AiBMyoEuaCFHraWVMUJRaez9a_AdeUIJEjCVH6BngjGBDA%3D%3D/playlist/index.m3u8"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
